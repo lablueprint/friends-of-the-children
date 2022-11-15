36621650bbcafef682d89ebfac46cb6ac2130871
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2022-11-14 20:38:21 -0800

    Created Example and Default pages

---
+++ src/pages/Example.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Example() {
+  return (
+    <div>
+      Example
+    </div>
+  );
+}
+
+export default Example;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Example from './Example';
+import Default from './Default';
+
+export { Example, Default };
