0545d7512bc681be06ad30cfc0798be884c6e4d8
erinkwon01 <erinkwon01@gmail.com>
2022-11-23 21:54:58 -0800

    DB update

--- src/pages/Modules.js
+++ src/pages/Modules.js
@@ -1,26 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import firebase from 'firebase/compat/app';
+import { db } from './firebase';
 import Module from '../components/Module';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
 
 function Modules() {
   const [modules, setModules] = useState([]);
 
   useEffect(() => {
-    firebase.firestore().collection('modules').get().then((sc) => {
+    db.collection('modules').get().then((sc) => {
       const module = [];
       sc.forEach((doc) => {
         const data = doc.data();
