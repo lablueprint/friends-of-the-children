e0f0fe07524bb6b384ebf58e5f6fd9c4e0f622c7
galenheuer <galenheuer@ucla.edu>
2022-11-23 21:15:49 -0800

    console.log()'ed username and password

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// const [password, setPassword] = useState('');
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(username);
+    console.log(password);
+  };
   return (
     <div>
       <form>
         <label htmlFor="username">
           Username:
           <br />
-          <input type="text" id="username" name="username" />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </label>
         <br />
         <label htmlFor="password">
           Password:
           <br />
-          <input type="text" id="password" name="password" />
+          <input
+            type="text"
+            id="password"
+            name="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </label>
         <br />
-        <input type="submit" value="Log in" />
+        <input type="submit" value="Log in" onClick={handleClick} />
       </form>
     </div>
   );
