89358aef142229396cee003d31be6372e412c37e
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2022-11-23 20:20:53 -0800

    Move firebase things to firebase.jsx

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,28 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-
-firebase.initializeApp(firebaseConfig);
+import { db } from './firebase';
 
 function Example() {
   const [profiles, setProfiles] = useState([]);
 
   useEffect(() => {
-    firebase.firestore().collection('profiles').get().then((sc) => {
+    db.collection('profiles').get().then((sc) => {
       const profile = [];
       sc.forEach((doc) => {
-        profile.push(doc.data());
+        const data = doc.data();
+        data.id = doc.id;
+        profile.push(data);
       });
 
       setProfiles(profile);
