9cb202cfd0908efaa59ff17a6876de618416cb7b
Joyce Chen <joyce.chen721@gmail.com>
2023-01-08 10:26:07 +0800

    create pinnable messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from '../styles/Messages.module.css';
+import { db } from '../pages/firebase';
 
 function Message(props) {
-  const { title, body } = props;
-  // const pinMessage = () => {
-  //   pinned = true;
-  // };
+  const { id, title, body } = props;
+  let pinned;
+  const [pinDesc, setDesc] = useState('📌 PIN');
+  const messageRef = db.collection('messages').doc(id);
+
+  const setPinned = () => {
+    messageRef.get().then((snap) => {
+      if (snap.exists) {
+        const data = snap.data();
+        pinned = data.pinned;
+        messageRef.set({
+          pinned: !pinned,
+        }, { merge: true });
+        pinned = !pinned;
+        if (pinned) setDesc('❗️📌 UNPIN');
+        else setDesc('📌 PIN');
+      } else {
+        console.log('Document does not exist');
+      }
+    });
+  };
+
   return (
     <div className={styles.message}>
       <h5>
@@ -19,16 +38,14 @@ function Message(props) {
         {' '}
         {body}
       </p>
-      <button type="button">pin 📌</button>
-      {/* <p>{date}</p> */}
+      <button type="button" onClick={(setPinned)}>{pinDesc}</button>
     </div>
   );
 }
 
 Message.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  // pinned: PropTypes.bool.isRequired,
-  // date: PropTypes.instanceOf(Date).isRequired,
 };
 export default Message;
