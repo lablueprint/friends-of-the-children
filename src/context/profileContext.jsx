973a5819a0aa691f576e7130fb33a0e972a1064e
Avnish Sengupta <avnishsengupta1@g.ucla.edu>
2023-02-11 22:06:03 -0800

    deleting public and src files outside client

--- src/context/profileContext.jsx
+++
@@ -1,6 +0,0 @@
-// we might use this for consistency throughout the app instead of using app.jsx state
-import { createContext } from 'react';
-
-const profileContext = createContext();
-
-export default profileContext;
--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-  return (
-    <div>
-      <h1>Page not accesible. Please login first!</h1>
-    </div>
-  );
-}
