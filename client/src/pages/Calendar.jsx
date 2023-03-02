661151bccf350508a52c03f481603ca6d9a93e56
Joyce Chen <joyce.chen721@gmail.com>
2023-03-01 23:44:22 -0800

    fix calendar reloading issue
    instead of using state hooks on input changes, just get the value of the input on form submission (using name attributes)

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,35 +1,13 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// export default Calendar;
-
-import { React, useState, createRef } from 'react';
+import { React, createRef } from 'react';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import interactionPlugin from '@fullcalendar/interaction'; // for selectable
 import styles from '../styles/Calendar.module.css';
 
-function renderEventContent(eventInfo) {
-  console.log(eventInfo);
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-      <p>{eventInfo.event.extendedProps.location}</p>
-      <p>
-        {eventInfo.event.start.toLocaleString()}
-        {' '}
-        -
-        {' '}
-        {eventInfo.event.end.toLocaleString()}
-      </p>
-
-    </>
-  );
-}
-
 function Calendar() {
+  const calendarRef = createRef();
+
   const handleEventClick = (eventInfo) => {
     eventInfo.jsEvent.preventDefault();
     // alert('a day has been clicked');
@@ -43,60 +21,63 @@ function Calendar() {
     console.log('End time: ', eventInfo.event.end);
   };
 
-  const calendarRef = createRef();
-
-  const [title, setTitle] = useState('');
-  // const [location, setLocation] = useState('');
-  // const [descrip, setDescrip] = useState('');
-  const [startTime, setStartTime] = useState('');
-  const [endTime, setEndTime] = useState('');
-
   const addEventFunc = (e) => {
     e.preventDefault();
+    const title = e.target.title.value;
+    const startTime = e.target.startTime.value;
+    const endTime = e.target.endTime.value;
     const eventArray = {
       title,
       start: startTime,
       end: endTime,
     };
+  
     const api = calendarRef.current.getApi();
     api.addEvent(eventArray);
+
+    const gapi = process.env.REACT_APP_FIREBASE_CALENDAR_ID;
+    console.log(gapi.client);
+    gapi.client.calendar.events.insert({
+      calendarId: 'fofthechildren@gmail.com',
+      resource: eventArray,
+    });
     console.log(eventArray);
   };
 
   return (
     <div className={styles.calendar}>
-      <FullCalendar
-        ref={calendarRef}
-        plugins={[dayGridPlugin, googleCalendarPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        googleCalendarApiKey={process.env.REACT_APP_FIREBASE_CALENDAR_ID}
-        events={{
-          googleCalendarId: 'fofthechildren@gmail.com',
-          className: 'gcal-event',
-        }}
-        selectable
-        eventClick={handleEventClick}
-        eventContent={renderEventContent}
-        showNonCurrentDates={false}
-      />
-
       <form onSubmit={(e) => addEventFunc(e)}>
         <h1>Upload Module</h1>
         Title:
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input type="text" name="title" />
         {/* Description:
         <input type="text" value={descrip} onChange={(e) => setDescrip(e.target.value)} />
         Location:
         <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} /> */}
         Start Time:
-        <input type="datetime-local" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+        <input type="datetime-local" name="startTime" />
         End Time:
-        <input type="datetime-local" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
-
+        <input type="datetime-local" name="endTime" />
         <button type="submit">Add Event</button>
-
       </form>
 
+      <FullCalendar
+        ref={calendarRef}
+        plugins={[dayGridPlugin, googleCalendarPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable
+        editable
+        selectMirror
+        dayMaxEvents
+        fixedWeekCount={false}
+        googleCalendarApiKey={process.env.REACT_APP_FIREBASE_CALENDAR_ID}
+        events={{
+          googleCalendarId: 'ayubali2443@gmail.com',
+          className: 'gcal-event',
+        }}
+        eventClick={handleEventClick}
+        // eventContent={renderEventContent}
+      />
     </div>
   );
 }
