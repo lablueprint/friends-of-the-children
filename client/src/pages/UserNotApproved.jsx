2f199a975b7046b439ac9c8f44cf935b36ba14a6
galenheuer <galenheuer@ucla.edu>
2023-05-17 22:54:38 -0700

    Update UserNotApproved.jsx

--- client/src/pages/UserNotApproved.jsx
+++ client/src/pages/UserNotApproved.jsx
@@ -1,23 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { useLocation, useNavigate, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../redux/sliceAuth';
-import * as api from '../api';
+import styles from '../styles/UserNotApproved.module.css';
+import LoginFamily from '../assets/images/login_family.svg';
 
 function UserNotApproved({ updateAppProfile, profile }) {
-  // const { state } = useLocation();
+  let pendingMessage = '';
   const dispatch = useDispatch();
-  // console.log(state);
+
   const handleLogout = () => {
     updateAppProfile(null);
     dispatch(logout(profile));
   };
+
+  if (profile.role.toLowerCase() === 'mentor') {
+    pendingMessage = 'Your request to become a mentor has been received and is awaiting approval. We will send you an email once your account has been approved.';
+  } else {
+    pendingMessage = 'Your request to become a caregiver has been received and is awaiting approval. We will send you an email once your account has been approved.';
+  }
+
   return (
     <div>
-      <h1>Waiting to approve you bestie</h1>
-      <Link to="/" onClick={handleLogout}> Log Out </Link>
+      <div>
+        <img src={LoginFamily} alt="fotc family" className={styles.family_img} />
+      </div>
+      <h1 className={styles.request_pending_message}>
+        {pendingMessage}
+      </h1>
+      <Link to="/" className={`${styles.btn_info} ${styles.btn_danger}`} onClick={handleLogout}> Log Out </Link>
     </div>
   );
 }
