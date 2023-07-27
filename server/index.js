ebc026a1ddf0e4b37fc7f8edd78c7b1916ac047f
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2023-07-26 21:45:21 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,6 +13,6 @@ app.use(cors());
 
 app.use('/', serverRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is listening on port:${PORT}`));
